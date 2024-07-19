76b1200b684e408890e37a448ae498df5709fa5b
Cold-chick <zaitkulov4@gmail.com>
2024-07-19 17:03:18 +0500

    add

--- resources/views/my_halls.blade.php
+++ resources/views/my_halls.blade.php
@@ -1,4 +1,118 @@
 <x-layout>
+    <style>
+        .hall_add {
+            width: 80%;
+            margin: 150px auto;
+        }
+    </style>
+    <div class="hall_add">
+
+        <div class="my-address contact-2">
+            <h3 class="heading-3">Добавить новый зал</h3>
+            <form action="{{ route('update_personal_data') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-lg-12 ">
+                        <div class="form-group name">
+                            <label>Название зала</label>
+                            <input type="text" name="name_hall" class="form-control" value="{{ Auth::user()->name }}">
+                        </div>
+                        @error('name_hall')
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>{{ $message }}</strong>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-lg-12 ">
+                        <div class="form-group number">
+                            <label>Площадь зала</label>
+                            <input type="text" name="area_hall" class="form-control"
+                                value="{{ Auth::user()->email }}">
+                        </div>
+                        @error('area_hall')
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>{{ $message }}</strong>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-lg-12 ">
+                        <div class="form-group subject">
+                            <label>Описание зала</label>
+                            <input id="description_hall" type="text" name="description_hall" class="form-control"
+                                value="{{ Auth::user()->phone }}">
+                        </div>
+                        @error('description_hall')
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>{{ $message }}</strong>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-lg-12 ">
+                        <div class="form-group photo">
+                            <label>Фото зала(чтобы несколько фото можно было выбрать)</label>
+                            <div class="input-group mb-3">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="photo_hall" accept="image/*"
+                                        id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                                    <label class="custom-file-label" for="inputGroupFile01">Выберите
+                                        файл/ы</label>
+                                </div>
+                            </div>
+                        </div>
+                        @error('photo_hall')
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>{{ $message }}</strong>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @enderror
+                    </div>
+                    <h4 style="margin-left: 15px;">Что есть в зале</h4>
+                    <div class="col-lg-12 ">
+                        <div class="form-group subject">
+                            <input id="coffee_hall" type="checkbox" name="coffee_hall">
+                            <label>
+                                <li><span class="icon flaticon-coffee-cup"></span> Кофе</li>
+                            </label>
+                            <input id="bar_hall" type="checkbox" name="bar_hall">
+                            <label>
+                                <li><span class="icon flaticon-wine-glass"></span> Мини-бар</li>
+                            </label>
+                            <input id="wifi_hall" type="checkbox" name="wifi_hall">
+                            <label>
+                                <li><span class="icon flaticon-wifi"></span> Wi-Fi</li>
+                            </label>
+                            <input id="tv_hall" type="checkbox" name="tv_hall">
+                            <label>
+                                <li><span class="icon flaticon-tv"></span> Телевизор</li>
+                            </label>
+                            <input id="lamp_hall" type="checkbox" name="lamp_hall">
+                            <label>
+                                <li><span class="icon flaticon-light"></span> Свет</li>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="send-btn">
+                            <button type="submit" class="theme-btn btn-style-one"><span
+                                    class="btn-title">Добавить</span></button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
 
     <!--Rooms Section-->
     <section class="rooms-section" style="margin-top: 40px">
@@ -9,8 +123,8 @@
                     data-wow-duration="1500ms">
                     <div class="inner-box">
                         <div class="image-box">
-                            <figure class="image"><a href="/hall"><img src="images/halls/IMG_5431.jpeg" alt=""
-                                        title=""></a></figure>
+                            <figure class="image"><a href="/hall"><img src="images/halls/IMG_5431.jpeg"
+                                        alt="" title=""></a></figure>
                         </div>
                         <div class="lower-box">
                             <h4>Зал Нефть</h4>
@@ -65,7 +179,8 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure class="image"><a href="room-single.html"><img
-                                        src="images/resource/featured-image-23.jpg" alt="" title=""></a>
+                                        src="images/resource/featured-image-23.jpg" alt=""
+                                        title=""></a>
                             </figure>
                         </div>
                         <div class="lower-box">
