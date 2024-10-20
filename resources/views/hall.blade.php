bd4c5b2889fca74deaf1930ac055bb00901ee051
XopK <dmahmutov12@gmail.com>
2024-10-20 15:44:30 +0500

    Update hall.blade.php

--- resources/views/hall.blade.php
+++ resources/views/hall.blade.php
@@ -5,7 +5,7 @@
         svg {
             cursor: pointer;
             overflow: visible;
-            width: 60px;
+            
 
             #heart {
                 transform-origin: center;
