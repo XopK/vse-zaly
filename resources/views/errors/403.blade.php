5f2d3d58b82727292fbccb27213d16c0b23ef9af
XopK <dmahmutov12@gmail.com>
2025-03-26 15:14:04 +0500

    errors

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Аренда залов в Уфе</title>
+    <!-- Stylesheets -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/jquery-ui.css">
+    <!-- Responsive File -->
+    <link href="/css/responsive.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="/images/favicon.png" type="image/x-icon">
+
+    <!-- Responsive Settings -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+    <!--[if lt IE 9]>
+    <script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body class="bg-light d-flex justify-content-center align-items-center vh-100">
+
+<div class="page-wrapper text-center p-5 rounded mt-0">
+    <h1 class="text-danger display-1">403</h1>
+    <p class="text-muted fs-5">Доступ запрещен. У вас нет прав для просмотра этой страницы.</p>
+    <a href="{{ url()->previous() }}" class="theme-btn btn-style-one"><span
+            class="btn-title">Назад</span></a>
+</div>
+
+<script src="/js/jquery.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/jquery-ui.js"></script>
+<script src="/js/jquery.fancybox.js"></script>
+<script src="/js/owl.js"></script>
+<script src="/js/scrollbar.js"></script>
+<script src="/js/appear.js"></script>
+<script src="/js/wow.js"></script>
+<script src="/js/custom-script.js"></script>
+
+</body>
+</html>
